dfc741b736200d5f1b771e889f1a617d142cfc26
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-16 21:11:53 +0530

    created empty components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,6 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'complete_angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('complete_angular');
   });
 
