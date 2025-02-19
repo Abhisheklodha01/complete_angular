e4e26023b764ed5f6d1a47d3a977c8298d5b611c
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-19 13:30:46 +0530

    control flow and routing in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { ControlFlowComponent } from './components/control-flow/control-flow.component';
 
 export const routes: Routes = [
     {
-       path: "home",
+       path: '',
        component: HomeComponent
     },
 
@@ -17,5 +18,10 @@ export const routes: Routes = [
     {
         path: 'admin',
         component: AdminComponent
+    },
+
+    {
+        path: 'control-flow',
+        component: ControlFlowComponent
     }
 ];
