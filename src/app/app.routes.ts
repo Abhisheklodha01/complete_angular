df8c3754c417869454e3255c17f4874258b4c857
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-19 10:28:21 +0530

    learning about how routing works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+       path: "home",
+       component: HomeComponent
+    },
+
+    {
+        path: 'user-profile',
+        component: UserComponent
+    },
+
+    {
+        path: 'admin',
+        component: AdminComponent
+    }
+];
